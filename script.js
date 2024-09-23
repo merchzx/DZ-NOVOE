8c17b960750dc496cac8257fbbdb019024cc0e17
ArtemRudomanenko <artemrudomanenko@gmail.com>
2024-09-23 17:19:24 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,114 @@
+//1
+// let a = (prompt("Input ur number ->"));
+
+// if(a%2==0) {
+//     alert("UR number is Even!!");
+// }
+// else {
+//     alert("Ur number is Odd!!")
+// }
+
+//2
+// let a,b;
+// a = (prompt("Input ur 1-st number ->"))
+// b = (prompt("Input ur 2-nd number ->"))
+// if(a>b) {
+//     alert("First number is bigger   " +a +">"+b)
+// }
+// else if(a<b) {
+//     alert("Second number is bigger   " +b +">"+a)
+// }
+// else if(a=b) {
+//     alert("they are identical    "+a+"="+b)
+// }
+
+//3
+// let a = (prompt("Input ur number ->"));
+// if(a<0) {
+//     alert("Ur number are negative  ")
+// }
+// else if(a>0) {
+//     alert("Ur number are positive  ")
+// }
+
+//4
+// let a,b,c;
+// a = (prompt("Input ur 1-st number ->"))
+// b = (prompt("Input ur 2-nd number ->"))
+// c = (prompt("Input ur 3-nd number ->"))
+// if(a>b && a>c) {
+//     alert("First number is bigger")
+// }
+// else if(a<b && b>c) {
+//     alert("Second number is bigger")
+// }
+// else if(a<c && b<c) {
+//     alert("Third number is bigger")
+// }
+// else{
+//     alert("they are identical")
+// }
+
+//5
+// let a,b,c;
+// c = (prompt("1) a + b  2) a – b  3) a * b  4) a / b.  ->"))
+// a = parseFloat(prompt("Input ur 1-st number ->"))
+// b = parseFloat(prompt("Input ur 2-nd number ->"))
+// if(c==1) {
+//     alert(a+b)
+// }
+// else if(c==2) {
+//     alert(a-b)
+// }
+// else if(c==3) {
+//     alert(a*b)
+// }
+// else if(c==4) {
+//     alert(a/b)
+// }
+// else {
+//     alert("TRY NEXT TIME!!")
+// }
+
+//6
+// let a = (prompt("Input ur number ->"));
+// if(a>1 &&a<30) {
+//     alert("Ur number between 1 and 30\t" + a)
+// }
+// else {
+//     alert("Ur number NOT between 1 and 30\t" + a)
+// }
+
+//7
+// let a = parseInt(prompt("Input ur 1  number ->"));
+// let b = parseInt(prompt("Input ur 2  number ->"));
+// if(a%b==0) {
+//     alert("X/Y - > Good")
+// }
+// else if(a%b!=0) {
+//     alert("X/Y - > BAD")
+// }
+
+//8
+// let a = parseInt(prompt("Input ur 1  number ->"));
+// if(a%3==0 && a%5==0 &&a%7==0) {
+//     alert("GOOOOD")
+// }
+// else {
+//     alert("BAD")
+// }
+
+//9
+// let a = parseInt(prompt("Input ur 1  number ->"));
+// alert(Math.abs(a))
+
+//10
+// let summ=0;
+// let a = prompt("Input ur number -> ");
+// let l = a.length;
+// alert(l);
+// for(let i=0;i<l;i++){
+    
+//     summ+=parseInt(a[i],20);
+// }
+// alert(summ);
